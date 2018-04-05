870eff780380fcb3b704e34176e127240fd0809b
charlesoma <charlyoma007@gmail.com>
2018-04-05 13:01:49 +0100

    Initial commit

---
+++ src/Contact.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+ 
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>GOT QUESTIONS?</h2>
+        <p>The easiest thing to do is to contact me
+        on <a href="http://github.com/charlesoma">Github</a>.
+        </p>
+      </div>
+    );
+  }
+}
+ 
+export default Contact;
